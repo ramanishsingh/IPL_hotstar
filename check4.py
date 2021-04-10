4479b87f7cd0c09fb6525236624a4597d0d5bc48
Ramanish Singh <rsthakur922@gmail.com>
2021-04-10 11:44:35 -0500

    delete unnecessary files

--- check4.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-cap = DesiredCapabilities().FIREFOX
-cap["marionette"] = False
-browser = webdriver.Firefox(capabilities=cap, executable_path="/bin/geckodriver")
-browser.get('http://google.com/')
-browser.quit()
--- check5.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-opts = Options()
-opts.binary = r"C:\Users\rstha\Documents\python_scripts\hotstar\FirefoxPortable\App\Firefox64\firefox.exe"
-
-driver = webdriver.Firefox(firefox_options=opts)
